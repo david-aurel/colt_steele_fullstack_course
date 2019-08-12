5b0e36f52c4370fe24dfe5d97c766b156e20d56b
David <53895961+david-aurel@users.noreply.github.com>
2019-08-12 22:34:21 +0200

    first commit

---
+++ weatherForecast.js
@@ -0,0 +1,21 @@
+// forecast for today in kelvin
+const kelvin = 150;
+
+// calculating celsius
+let celsius = kelvin - 273;
+
+// calculating fahrenheit
+let fahrenheit = celsius * (9/5) + 32;
+
+// rounding the result of the fahrenheit calculation down
+fahrenheit = Math.floor(fahrenheit);
+
+// printing
+console.log(`The temperature today is about ${fahrenheit} degrees Fahrenheit.`);
+
+// extra Practice: convert celsius to newton
+let newton = celsius * (33/100);
+newton = Math.floor(newton);
+
+// printing again
+console.log(`In Newton, that would be about ${newton} Newton.`);
