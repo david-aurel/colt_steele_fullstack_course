ab9cfd2d7845429167794f9eb99d86db7832d291
David <53895961+david-aurel@users.noreply.github.com>
2019-09-05 10:54:00 +0200

    replace with else if statements

--- Simple Guessing Game/script.js
+++ Simple Guessing Game/script.js
@@ -1,18 +1,22 @@
 var age = Number(prompt("Whats your age?"));
 
-if (age > 0) {
+if (age < 0) {
     console.log("Your age is negative?")
 }
 
-if (age === 21) {
+else if (age === 21) {
     console.log("Happy birthday, you get a free shot!");
 }
 
-if (age % 2 !== 0) {
-    console.log("Your age is odd...");
+else if (age % 2 !== 0) {
+    console.log("Your age is odd!");
 }
 
-if (age % Math.sqrt(age) === 0) {
+else if (age % Math.sqrt(age) === 0) {
     console.log("Your age is a perfect square!");
+}
+
+else {
+    console.log("Your age is " + age + ".")
 }
 
