bf3250d65e86ed04cf2749fb858dbefe26b4ad95
David <53895961+david-aurel@users.noreply.github.com>
2019-09-09 20:37:15 +0200

    finish exercise

---
+++ DOM-Manipulation/SimpleManipulationExercise/script.js
@@ -0,0 +1,8 @@
+console.log("connected")
+
+var btn = document.querySelector("button");
+var body = document.querySelector("body");
+
+btn.addEventListener("click", function(){
+    body.style.background = "purple";
+});
