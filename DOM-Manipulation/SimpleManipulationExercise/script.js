c6c246cf8ff93e20a31f246f27ce635a1f484bdf
David <53895961+david-aurel@users.noreply.github.com>
2019-09-09 20:48:10 +0200

    improve

--- DOM-Manipulation/SimpleManipulationExercise/script.js
+++ DOM-Manipulation/SimpleManipulationExercise/script.js
@@ -2,7 +2,28 @@ console.log("connected")
 
 var btn = document.querySelector("button");
 var body = document.querySelector("body");
+var isColored = false;
+
 
 btn.addEventListener("click", function(){
-    body.style.background = "purple";
+    if (isColored === false){
+        body.style.background = "purple";
+        isColored = true;
+    }
+    else {
+        body.style.background = "white";
+        isColored = false;
+    }
 });
+// var button = document.querySelector("button");
+// var isColored = false;
+
+// btn.addEventListener("click", function(){
+// if(isColored){
+// document.body.style.background = "white";
+// isColored = false;
+// } else{
+// document.body.style.background = "blue";
+// isColored = true;
+// }
+// }); 
