1bdc8f9936e45975464cbfe85a8b1f4e216863d3
David <53895961+david-aurel@users.noreply.github.com>
2019-09-09 21:26:44 +0200

    add solution

--- DOM-Manipulation/SimpleManipulationExercise/script.js
+++ DOM-Manipulation/SimpleManipulationExercise/script.js
@@ -15,3 +15,10 @@ btn.addEventListener("click", function(){
         isColored = false;
     }
 });
+
+
+//if you want to make it shorter, use classList.toggle and add some css in the stylesheets. Just toggle a class on the element on or off when it's clicked and define the class elsewhere (in th stylesheet) to change color.
+
+btn.addEventListener("click", function(){
+    body.classList.toggle("purple");
+})
