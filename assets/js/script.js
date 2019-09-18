75d13cb0318afc85a5cbd872fc080108dfb2cfda
David <53895961+david-aurel@users.noreply.github.com>
2019-09-18 15:53:58 +0200

    add functionality to add new todos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,23 @@
 //check off specific todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
-}); 
+});
 
 //click on x to delete todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(function(){
-        $(this).remove()
+        $(this).remove();
     });
     event.stopPropagation();
+});
+
+//type and hit enter to add a new todo
+$("input[type='text']").keypress(function(event){
+    if (event.which === 13) {
+        //grabbing new input text
+       var todoText = $(this).val();
+       $(this).val("");
+       //create new li and add to ul
+       $("ul").append("<li><span>X</span> " + todoText + " </li>")
+    };
 });
