15c659b5f084fe4dd8803ca910517163b1cdd986
david-aurel <david.a.durlan@gmail.com>
2019-09-24 11:02:48 +0000

    first commit

---
+++ IntroToNode/echo.js
@@ -0,0 +1,8 @@
+function echo(word, times){
+	for (var i = 0; i < times; i++){
+		console.log(word);
+	};
+};
+
+echo("Echo!!!", 10) //should print "Echo!!!" 10 times
+echo("Tater Tots", 3) //should print "Tater Tots" 3 times
