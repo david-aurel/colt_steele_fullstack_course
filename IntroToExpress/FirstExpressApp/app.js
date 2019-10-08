b5c8dd10973b4c6a44bc2931f6340bdf10e7def7
David <53895961+david-aurel@users.noreply.github.com>
2019-10-08 16:24:19 +0200

    add routes

--- IntroToExpress/FirstExpressApp/app.js
+++ IntroToExpress/FirstExpressApp/app.js
@@ -1,20 +1,45 @@
 let express = require('express');
 let app = express();
 
+
 // "/" => "Hi there!"
 app.get("/", function(req, res){
     res.send("Hi there!");
 });
 
+
 // "/bye" => "Goodbye!"
-    app.get("/bye", function(req, res){
-        res.send("Goodbye!");
-    });
+app.get("/bye", function(req, res){
+    res.send("Goodbye!");
+});
 
 
 // "/dog" => "Meow!"
 app.get("/dog", function(req, res){
     res.send("Meow!");
+});
+
+
+// "/test" => "test!"
+app.get("/test", function(req, res){
+    res.send("test!");
+});
+
+
+//subreddit
+app.get("/r/:subredditName", function(req, res){
+    let subreddit = req.params.subredditName;
+    res.send("Welcome to the " +subreddit+ " subreddit!");
+});
+
+app.get("/r/:subredditName/comments/:id/:title", function(req, res){
+    res.send("Welcome to the comments page!");
+});
+
+
+// //catchall
+app.get("*", function(req, res){
+    res.send("You are a star!")
 });
 
 
