e0ba6596c42ee08e1bb6ed010434a773139b140d
David <53895961+david-aurel@users.noreply.github.com>
2019-10-11 14:52:35 +0200

    add requset markup

---
+++ APIs/first_api_request.js
@@ -0,0 +1,8 @@
+let request = require("request");
+
+request('https://jsonplaceholder.typicode.com/users/1', (error, response, body) => {
+    if(!error && response.statusCode === 200){
+        let parsedData = JSON.parse(body);
+        console.log(`${parsedData.name} lives in ${parsedData.address.city}.`);
+    };
+});
