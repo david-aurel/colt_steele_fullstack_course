772e3c3fa447b589e093876e99176fe0efbed475
David <53895961+david-aurel@users.noreply.github.com>
2019-10-11 16:00:34 +0200

    add logic

--- APIs/movie_search_app/app.js
+++ APIs/movie_search_app/app.js
@@ -4,14 +4,20 @@ const request = require('request');
 
 app.set('view engine', 'ejs');
 
+//search page
+app.get('/', (req, res) => {
+    res.render('search');
+});
+
+// results page
 app.get('/results', function(req, res){
-    request('http://www.omdbapi.com/?s=harry&apikey=thewdb', (error, response, body) => {
+    let query = req.query.search;
+    let url = 'http://www.omdbapi.com/?s=' + query +  '&apikey=thewdb'
+    request( url, (error, response, body) => {
     if(!error && response.statusCode === 200);
     let data = JSON.parse(body);
     res.render('results', {data: data});
-
     });
-
 }); 
 
 
