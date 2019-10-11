01d3d16b476bcfc23e0dc437d02d8984385a6b7d
David <53895961+david-aurel@users.noreply.github.com>
2019-10-11 13:30:44 +0200

    add second part of the post request

--- Intermediate Express/PostRequestDemo/app.js
+++ Intermediate Express/PostRequestDemo/app.js
@@ -1,10 +1,13 @@
 let express = require("express");
 let app = express();
+let bodyParser = require("body-parser"); 
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+ // friends list data
+ let friends = ["Tony", "Miranda", "Justin", "Pierre", "Lilly"]
 
-// routes
 
 // root
 app.get("/", function(req, res){
@@ -13,15 +16,15 @@ app.get("/", function(req, res){
 
 // friends list
 app.get("/friends", function(req, res){
-    // friends list data
-    let friends = ["Tony", "Miranda", "Justin", "Pierre", "Lilly"]
     // put out the template and pass the friends object 
     res.render("friends", {friends: friends})
 });
 
 // post request for adding friends
 app.post("/addfriend", function(req, res){
-    res.send("You have reached the post request!")
+    let newFriend = req.body.newFriend;
+    friends.push(newFriend);
+    res.redirect("friends");
 });
 
 
