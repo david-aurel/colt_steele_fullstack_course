bb718bb59187a92e5fb21c6572c004db24f3a2fe
David <53895961+david-aurel@users.noreply.github.com>
2019-10-11 13:16:48 +0200

    add first part of the post request

--- Intermediate Express/PostRequestDemo/app.js
+++ Intermediate Express/PostRequestDemo/app.js
@@ -11,6 +11,20 @@ app.get("/", function(req, res){
     res.render("home")
 });
 
+// friends list
+app.get("/friends", function(req, res){
+    // friends list data
+    let friends = ["Tony", "Miranda", "Justin", "Pierre", "Lilly"]
+    // put out the template and pass the friends object 
+    res.render("friends", {friends: friends})
+});
+
+// post request for adding friends
+app.post("/addfriend", function(req, res){
+    res.send("You have reached the post request!")
+});
+
+
 // listening on port localhost:3000
 app.listen(3000, function(){
     console.log("Server started!")
